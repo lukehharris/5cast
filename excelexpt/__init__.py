f460439ad4b75500c066f4072e17ef2150ec2df4
Luke Harris <lukehharris@gmail.com>
2013-04-29 00:13:54 -0400

    email errors

--- excelexpt/__init__.py
+++ excelexpt/__init__.py
@@ -18,6 +18,30 @@ login_manager.setup_app(app)
 app.secret_key = "DS53DFS3DF\SDF5SDF658DS5F2SD1F\2SD1F32SD1"
 
 
+ADMINS = ['lukehharris@gmail.com']
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    mail_handler = SMTPHandler('127.0.0.1',
+                               'server-error@modelbuilder.com',
+                               ADMINS, 'ModelBuilder Failed')
+    mail_handler.setLevel(logging.ERROR)
+
+    from logging import Formatter
+    mail_handler.setFormatter(Formatter('''
+    Message type:       %(levelname)s
+    Location:           %(pathname)s:%(lineno)d
+    Module:             %(module)s
+    Function:           %(funcName)s
+    Time:               %(asctime)s
+
+    Message:
+
+    %(message)s
+    '''))
+
+    app.logger.addHandler(mail_handler)
+
 import excelexpt.views
 
     
